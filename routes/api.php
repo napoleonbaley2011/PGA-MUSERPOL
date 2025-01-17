401a473f9e110f70e06060a7468cc926d01d7591
napeleonbalety2011 <lchamacal@fcpn.edu.bo>
2025-01-17 17:07:29 -0400

    Correcciones a las sumas de los productos en el reporte de fisico valorado y consolidado

--- routes/api.php
+++ routes/api.php
@@ -75,12 +75,12 @@ Route::group([
         Route::get('/PrintKardex/{material}', [ReportController::class, 'print_kardex']);
         Route::get('/ReportPrintValuedPhysical', [ReportController::class, 'ValuedPhysical']);
 
-        Route::get('/ReportPrintValuedPhysical2', [ReportController::class, 'ValuedPhysicalPrevis']);
-        Route::get('/ReportPrintValuedPhysical23/{management}', [ReportController::class, 'consolidated_inventory']);
+        
+        Route::get('/ReportPrintValuedPhysicalConsolidated/{management}', [ReportController::class, 'consolidated_inventory']);
 
 
         Route::get('/PrintValuedPhysical', [ReportController::class, 'PrintValuedPhysical']);
-        Route::get('/ReportPrintValuedPhysicalConsolidated/{management}', [ReportController::class, 'consolidated_valued_physical_inventory']);
+        
         Route::get('/PrintValuedPhysicalConsolidated/{management}', [ReportController::class, 'print_consolidated_valued_physical_inventory']);
         Route::get('/ManagementClosure', [ReportController::class, 'management_closure']);
 
